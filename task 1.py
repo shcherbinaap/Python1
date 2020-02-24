adea4485f701ab0c991528a0be5e5a99780819b7
shcherbinaap <sherbus@mail.ru>
2020-02-24 18:35:19 +0300

    Домашнее задание урок 1

---
+++ task 1.py
@@ -0,0 +1,28 @@
+#1. Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+a = 5
+b = "Bob"
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+user_num_1 = int(input("Введите первое число: "))
+user_num_2 = int(input("Введите второе число: "))
+user_str_1 = input("Введите свое первое предложение: ")
+user_str_2 = input("Введите свое второе предложение: ")
+
+print(user_num_1)
+print(type(user_num_1))
+
+print(user_num_2)
+print(type(user_num_2))
+
+print(user_str_1)
+print(type(user_str_1))
+
+print(user_str_2)
+print(type(user_str_2))
